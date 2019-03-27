156ffcdfdeb60f198b27d2180b403a03d865d331
Isabel Porcuna <isabelporcuna@isabels-macbook-pro.local>
2019-03-27 13:35:32 +0100

    item array converted into class Item

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -12,16 +12,18 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["L1.0","L2.0","L3.0"]
     
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
         return itemArray.count
     }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
         cell.textLabel?.text = itemArray[indexPath.row]
-    
     
         return cell
     }
@@ -64,6 +66,7 @@ class TodoListViewController: UITableViewController {
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
+            
             textField = alertTextField
         }
         
@@ -78,7 +81,5 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    
 }
 
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Isabel Porcuna on 3/27/19.
+//  Copyright © 2019 Isabel Porcuna. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
