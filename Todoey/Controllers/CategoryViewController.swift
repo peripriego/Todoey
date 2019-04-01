e23b4048df104e041f9913d775f123104bbf9a83
Isabel Porcuna <isabelporcuna@isabels-macbook-pro.local>
2019-04-01 12:38:56 +0200

    Persisting using CoreData

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -11,16 +11,98 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadCategories()
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "gotoItems", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let destinationVC = segue.destination as! TodoListViewController
+       
+        if let indexPath = tableView.indexPathForSelectedRow {
+            
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
+    
+    func loadCategories() {
+        
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+        categories = try context.fetch(request)
+        } catch {
+        }
+        tableView.reloadData()
+        
+    }
+    
+    func saveCategories() {
+        do {
+        try context.save()
+        } catch {
+            print("Error saving category: \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    
+    
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+    var textField = UITextField()
+    
+    let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+    
+    let action = UIAlertAction(title: "Add", style: .default) { (action) in
+        
+        let newCategory = Category(context: self.context)
+        newCategory.name = textField.text!
+        
+        self.categories.append(newCategory)
+        self.saveCategories()
+        
+        
+    }
+   
+       alert.addAction(action)
+        alert.addTextField { (field) in
+                textField = field
+            textField.placeholder = "Add new category"
+        }
+        
+        present(alert, animated: true, completion: nil)
     }
     
-   
-    
+
     
     
     
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -15,6 +15,13 @@ class TodoListViewController: UITableViewController {
 
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    var selectedCategory : Category? {
+        
+        didSet {
+            loadItems()
+        }
+        
+    }
     
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -66,6 +73,8 @@ class TodoListViewController: UITableViewController {
             
             newItem.done = false
             
+            newItem.parentCategory = self.selectedCategory
+            
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -96,7 +105,17 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        }
+        else {
+            request.predicate = categoryPredicate
+        }
+        
         
         do {
         itemArray = try context.fetch(request)
@@ -113,7 +132,6 @@ class TodoListViewController: UITableViewController {
 
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
        
-        loadItems()
     }
 }
 
@@ -125,11 +143,11 @@ extension TodoListViewController : UISearchBarDelegate {
         
         let request : NSFetchRequest<Item> = Item.fetchRequest()
         
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
 //
     }
     
